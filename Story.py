c7102cb6eb67c4f3c2c06e893fd3871050e07fdd
ghazigamer <46258765+ghazigamer@users.noreply.github.com>
2020-06-28 02:03:16 +1200

    Update Story.py

--- Story.py
+++ Story.py
@@ -1,14 +1,15 @@
 import time
 from Player import Player
+from colored import stylize
 P = Player()
 
 
 class Story:
     def __init__(self):
-        self.the_beginning = ("In a cave, you were born and around you there are too many books and the 'Holy Sword' , you have learnt about  each era of this world by reading books....though you dont know, which era are you living. You have learnt how to use the 'Holy Sword'.\nOne day, you were going to read one last book before taking a journey in this vast cave,in this book you found a note:\n'Please save me from the monsters'\n-The Princess. \nYour instinct tells you to save her, and like this you started your journey. ")
-        self.step2 = (
-            "\nYou are walking in the cave, trying to collect your bravery , you see an enemy ,you gotta walk more to get him and kill him.")
-        self.step4 = ("\nYou defeated the enemy, you are so happy that your time that went on reading books as a kid was not a waste!, you start walking more to face a bigger threat.")
+        self.the_beginning = (stylize("\nIn a cave, you were born and around you there are too many books and the 'Holy Sword' , you have learnt about  each era of this world by reading books....though you dont know, which era are you living. You have learnt how to use the 'Holy Sword'.\n\nOne day, you were going to read one last book before taking a journey in this vast cave,in this book you found a note:\n'Please save me from the monsters'\n-The Princess. \n\nYour instinct tells you to save her, and like this you started your journey. ",colored.fg('dark_gray')))
+        self.step2 = (stylize(
+            "\nYou are walking in the cave, trying to collect your bravery , you see an enemy ,you gotta walk more to get him and kill him.",colored.fg('indian_red_1a')))
+        self.step4 = ("\nYou defeated the enemy, you are so happy that your time that went on reading books as a kid was not a waste!, you start walking more to face a bigger threat.\n")
         self.step13 = (
             "\nYou climb up the stair and you see a new area that you have never seen, it is dark ,and so your mind....because you dont know your reality yet.")
         self.step20 = (
