0906205c1d1aa229af40c0224a7f83a43b63884a
ghazigamer <46258765+ghazigamer@users.noreply.github.com>
2020-06-29 07:18:30 +1200

    Update Start-Game.py

--- Start-Game.py
+++ Start-Game.py
@@ -1,3 +1,7 @@
+import sys
+from Output_Unbuffering import Unbuffered
+sys.stdout = Unbuffered(sys.stdout)
 from Operations import Operations
-O = Operations()
-O.ask_player()
+if __name__=='__main__':
+    O = Operations()
+    O.ask_player()
