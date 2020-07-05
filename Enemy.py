44f536470ff725258bf0f4d3891864cf923528a9
ghazigamer <46258765+ghazigamer@users.noreply.github.com>
2020-07-06 06:02:41 +1200

    Delete Enemy.py

--- Enemy.py
+++
@@ -1,5 +0,0 @@
-
-class Enemy:
-    def __init__(self):
-        self.enemies = ["Bandit", "Witch", "Stone Monster", "Devassin"]
-        self.boss = ["SHIELDOS", "SPARTAN", "BLODAS", "MAGIC-FATHER"]
