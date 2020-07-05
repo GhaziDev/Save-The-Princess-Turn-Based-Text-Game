2c2d92e5f13949201015030e34eb627bfeb8c4f7
ghazigamer <46258765+ghazigamer@users.noreply.github.com>
2020-07-06 06:03:17 +1200

    Delete Start-Game.py

--- Start-Game.py
+++
@@ -1,7 +0,0 @@
-import sys
-from Output_Unbuffering import Unbuffered
-sys.stdout = Unbuffered(sys.stdout)
-from Operations import Operations
-if __name__=='__main__':
-    O = Operations()
-    O.ask_player()
