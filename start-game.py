48126feb820bc2c7664e4d34ac6fee176460027c
ghazigamer <46258765+ghazigamer@users.noreply.github.com>
2020-07-07 19:34:25 +0300

    Update start-game.py

--- start-game.py
+++ start-game.py
@@ -1,10 +1,10 @@
 import sys
 from output_unbuffering import Unbuffered
-sys.stdout = Unbuffered(sys.stdout)
+
 from operations import Operations
 from role import Role
 
 if __name__=='__main__':
-    r=Role()
+    sys.stdout = Unbuffered(sys.stdout)
     O = Operations()
     O.ask_player()
